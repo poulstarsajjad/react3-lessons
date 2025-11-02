388b2447dc6e5ae781793518863745e097846eb1
Sajjad <askari.sajjad.1993@gmail.com>
2025-11-02 15:58:39 +0330

    add web address

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ function App() {
   return (
     <div>
       <h1 className="text-gray-600 bg-red-200 p-5 font-bold text-5xl">forza milan</h1>
+      <p>
+        https://www.figma.com/community/file/1331381641303792117/furniture-ecommerce-website-ui
+      </p>
     </div>
   )
 }
